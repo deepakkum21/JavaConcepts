b3d85a47be4843a81e2f68728c6b3c1286fd21ab
Deepak Kumar <Deepak.Kumar@sysbiz.org>
2019-06-10 10:37:36 +0530

    method parameter reflection added

---
+++ java8/MethodParameterReflection/src/main/java/com/deepak/java8/methodparameterreflection/Calculate.java
@@ -0,0 +1,14 @@
+package com.deepak.java8.methodparameterreflection;
+
+public class Calculate {
+	int add(int a, int b) {
+		return (a + b);
+	}
+
+	int mul(int a, int b) {
+		return (b * a);
+	}
+}
+
+// before compiling and executing below code, first compile Calculate class by following command:
+// javac -parameters Calculate.java  
